b2a70bde8252ecce3b9746d5d6f2291a8f09eec8
atrislab <atrislab@gmail.com>
2023-07-04 14:14:38 +0200

    ready to push

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# todolist00 Primer intento de:
+## todolist00 
+
+Primer intento de:
 - crear un proyecto react + vite
 - crear una todo list
+
+## Setup:
+
+- Hemos empezado por crear una carpeta de proyecto con el nombre deseado.
+- Debemos de tener node y npm instalados previamente.
+    Podemos comprobarlo usando "node -v" y "npm -v" en la consola.
+    En nuestro caso tenemos ambos instalados, asi que procedemos al siguiente paso.
+- En la documentación de Vite (https://vitejs.dev/guide/) en la sección Getting Started,
+    aparecen diversas formas de instalación.
+    Nosotros usaremos el método npm.
+    Ejecutamos en la consola : "npm create vite@latest"
+    y elegimos, de las opciones:
+    - el nombre del proyecto 
+    - react como framework
+    - javascript como "variante"
+
+- Instalamos el resto de paquetes con npm install
+    - Para saber qué se ha instalado podemos usar el comando 2npm ls".
+
+- Si todo ha salido bien, tenemos una vista al proyecto desde:
+"http://localhost:5173/"
+
+- Instalaremos en las dependencias de desarrollador vitest
+"npm install -D vitest". (Because it's free.)
+ - Lo añadimos a los scripts de nuestro package.json.
+
+
+## Cómo molaría tener tiempo...
+
+- Pero va a ser que no
+- No dispongo de tiempo (ahora) para currarme un readme guapo
+- Se han de añadir mejoras y nuevas funcionalidades
+- Pero hasta aquí , no está mal.
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTasks([...tasks, newTask]);
+    setNewTask('');
+  };
+
+  const handleDelete = (index) => {
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
+  };
+
+  const handleClear = () => {
+    setTasks([]);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <body>
+    <header>
+      <h1>Lista de Tareas</h1>
+    </header>
+    <main>
+      <form id='form' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          id='textinput'
+          placeholder='Add'
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button className='button' id='submitbutton' type="submit">Agregar tarea</button>
+      </form>
+      <ul className='list'>
+        {tasks.map((task, index) => (
+          <li className='lielement' key={index}>
+            {task}
+            <button className='button' id='deletebutton' onClick={() => handleDelete(index)}>Borrar</button>
+          </li >
+        ))}
+      </ul>
+      <button className='button' id='clearbutton' onClick={handleClear}>Borrar todas las tareas</button>
+    </main>
+    <footer>
+      <p>He de mejorarlo con las nuevas indicaciones</p>
+      <p>y mejorar los estilos</p>
+    </footer>
+  </body>
+  );
 }
 
-export default App
+export default App;
